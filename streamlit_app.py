0fb4ce894672d3ad39efa713dc37ae2fb44b89fa
q1w2e3r4dd <h098s02716@chunchon.gwe.hs.kr>
2025-11-11 11:17:13 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,7 +70,7 @@ st.markdown("---")
 CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     
      labels[0]: {
-       "texts": ["손은 손톱이 있습니다."],
+       "texts": ["눈은 눈꼽이 있습니다."],
     #   "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
     #   "videos": ["https://youtu.be/XXXXXXXXXXX"]
      },
