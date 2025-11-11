2f3cfcc8cdc7f703847c47224b58712037be7133
q1w2e3r4dd <h098s02716@chunchon.gwe.hs.kr>
2025-11-11 11:18:23 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
      labels[0]: {
        "texts": ["눈은 눈꼽이 있습니다."],
     #   "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
-    #   "videos": ["https://youtu.be/XXXXXXXXXXX"]
+       "videos": ["https://www.youtube.com/watch?v=XERplfomyFs"]
      },
 }
 
